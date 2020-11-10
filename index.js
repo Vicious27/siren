2354fd4c401637cbb69462ff2226efc6f65fe893
Raul Garcia <punkvomit21@gmail.com>
2020-11-10 06:33:23 -0500

    added showdata and getmoresongs pagination function

--- index.js
+++ index.js
@@ -15,9 +15,40 @@ async function searchSongs(term) {
 }
 
 // Show song and artist in DOM
-function showData() {
+function showData(data) {
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data.map(song => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}"
+      data-songtitle="${song.title}">Get Lyrics</button>
+      </li>`
+  )
+      .join('')}
+    </ul>  
+  `;
 
+  if (data.pre || data.next) {
+    more.innerHTML = `
+    ${data.prev ? `<button class="btn" onclick="getMoreSongs
+    ('${data.prev}')">Prev</button>` : ''}
+    ${data.next ? `<button class="btn" onclick="getMoreSongs ('${data.next}')">Next</button>` : ''}
+    `;
+  } else {
+    more.innerHTML = '';
+  }
 }
+
+// Get Prev and next song results using a cors proxy
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+
+
 
 // Event Listeners
 form.addEventListener('submit', e => {
