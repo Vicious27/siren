93d422d4b9a8c8a7a34b56afed2d4b37cbc38da8
Raul Garcia <punkvomit21@gmail.com>
2020-11-10 04:54:33 -0500

    added asunc await function making api call

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
 
-// Event Listeners
+// Search by song or artist
+async function searchSongs(term) {
 
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Show song and artist in DOM
+function showData() {
+
+}
+
+// Event Listeners
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
 
   if (!searchTerm) {
-    alert('Please type in a search term');
+    alert('Please, Type in a search term');
   }
   else {
     searchSongs(searchTerm);
