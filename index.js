8e3e19b2669b39358bc6e5b9e0b6c47d87f69908
Raul Garcia <punkvomit21@gmail.com>
2020-11-10 04:07:55 -0500

    added js eventlistener on submit with searchterm validation

---
+++ index.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Event Listeners
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert('Please type in a search term');
+  }
+  else {
+    searchSongs(searchTerm);
+  }
+});
